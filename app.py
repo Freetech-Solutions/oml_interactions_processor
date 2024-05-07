3dbf3789700e2cbee4964900feb306f7db2abf35
fpignataro <fabian.pignataro84@gmail.com>
2024-05-07 11:00:20 -0300

    Tx & Rx split & s3-aws IAM scenary

--- app.py
+++ app.py
@@ -127,14 +127,20 @@ def callback(ch, method, properties, body):
     split_channels = message.get('splitChannels', 'False') == 'True'
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1dc3414 (Tx & Rx split & s3-aws IAM scenary)
     if split_channels == True:
         process_audio_file(file_name, date_file_name, process_split=True)
     else:
         process_audio_file(file_name, date_file_name)
 
+<<<<<<< HEAD
 =======
     process_audio_file(file_name, date_file_name, process_split=split_channels)
 >>>>>>> 58b2b34 (fix)
+=======
+>>>>>>> 1dc3414 (Tx & Rx split & s3-aws IAM scenary)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def consume():
